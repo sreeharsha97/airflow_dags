0bf8add770fa5b7cd7715110dc190d8bee8341a0
SreeHarsha Katta <sreeharsha97@gmail.com>
2020-11-06 15:30:44 +0530

    Update example_python_operator_airflow.py

--- example_python_operator_airflow.py
+++ example_python_operator_airflow.py
@@ -18,6 +18,7 @@ def sample_python_function(**kwargs):
     """
     Insert arbitrary python function here
     """
+    print("Dummy Function Test")
 
 dag = DAG('example_python_operator_airflow',
             max_active_runs=3,
@@ -29,7 +30,7 @@ with dag:
 
     start = DummyOperator(task_id='start')
 
-    for i in range(0,10):
+    for i in range(0,2):
 
         t1 = PythonOperator( 
             task_id='python_function_af_{0}'.format(i),
