269d757b2ad7b80d3394017fcf7beb8e6ed5b31d
SreeHarsha Katta <sreeharsha97@gmail.com>
2020-11-06 16:03:50 +0530

    Update example_python_operator_airflow.py

--- example_python_operator_airflow.py
+++ example_python_operator_airflow.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -16,30 +17,32 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""Example DAG demonstrating the usage of the PythonOperator."""
+from __future__ import print_function
+
 import time
+from builtins import range
 from pprint import pprint
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.utils.dates import days_ago
 
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
 args = {
-    'owner': 'airflow',
+    'owner': 'Airflow',
+    'start_date': days_ago(2),
 }
 
 dag = DAG(
     dag_id='example_python_operator_airflow',
     default_args=args,
     schedule_interval=None,
-    start_date=days_ago(2),
-    tags=['example'],
+    tags=['example']
 )
 
 
 # [START howto_operator_python]
 def print_context(ds, **kwargs):
-    """Print the Airflow context and ds variable from the context."""
     pprint(kwargs)
     print(ds)
     return 'Whatever you return gets printed in the logs'
@@ -47,6 +50,7 @@ def print_context(ds, **kwargs):
 
 run_this = PythonOperator(
     task_id='print_the_context',
+    provide_context=True,
     python_callable=print_context,
     dag=dag,
 )
@@ -60,7 +64,7 @@ def my_sleeping_function(random_base):
 
 
 # Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
-for i in range(3):
+for i in range(5):
     task = PythonOperator(
         task_id='sleep_for_' + str(i),
         python_callable=my_sleeping_function,
@@ -70,34 +74,3 @@ for i in range(3):
 
     run_this >> task
 # [END howto_operator_python_kwargs]
-
-
-# [START howto_operator_python_venv]
-def callable_virtualenv():
-    """
-    Example function that will be performed in a virtual environment.
-    Importing at the module level ensures that it will not attempt to import the
-    library before it is installed.
-    """
-    from time import sleep
-
-    from colorama import Back, Fore, Style
-
-    print(Fore.RED + 'some red text')
-    print(Back.GREEN + 'and with a green background')
-    print(Style.DIM + 'and in dim text')
-    print(Style.RESET_ALL)
-    for _ in range(10):
-        print(Style.DIM + 'Please wait...', flush=True)
-        sleep(10)
-    print('Finished')
-
-
-virtualenv_task = PythonVirtualenvOperator(
-    task_id="virtualenv_python",
-    python_callable=callable_virtualenv,
-    requirements=["colorama==0.4.0"],
-    system_site_packages=False,
-    dag=dag,
-)
-# [END howto_operator_python_venv]
