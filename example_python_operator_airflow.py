10b1586b0ad059c1d9c2ece2805954f0d7084923
SreeHarsha Katta <sreeharsha97@gmail.com>
2020-11-06 15:56:28 +0530

    Update example_python_operator_airflow.py

--- example_python_operator_airflow.py
+++ example_python_operator_airflow.py
@@ -1,36 +1,103 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Example DAG demonstrating the usage of the PythonOperator."""
+import time
+from pprint import pprint
+
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.utils.dates import days_ago
 
-default_args = {
-    'owner': 'airflow'
+args = {
+    'owner': 'airflow',
 }
 
-def sample_python_function(**kwargs):
+dag = DAG(
+    dag_id='example_python_operator_airflow',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+
+# [START howto_operator_python]
+def print_context(ds, **kwargs):
+    """Print the Airflow context and ds variable from the context."""
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    python_callable=print_context,
+    dag=dag,
+)
+# [END howto_operator_python]
+
+
+# [START howto_operator_python_kwargs]
+def my_sleeping_function(random_base):
+    """This is a function that will run within the DAG execution"""
+    time.sleep(random_base)
+
+
+# Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
+for i in range(3):
+    task = PythonOperator(
+        task_id='sleep_for_' + str(i),
+        python_callable=my_sleeping_function,
+        op_kwargs={'random_base': float(i) / 10},
+        dag=dag,
+    )
+
+    run_this >> task
+# [END howto_operator_python_kwargs]
+
+
+# [START howto_operator_python_venv]
+def callable_virtualenv():
     """
-    Insert arbitrary python function here
+    Example function that will be performed in a virtual environment.
+    Importing at the module level ensures that it will not attempt to import the
+    library before it is installed.
     """
-    print("Dummy Function Test")
+    from time import sleep
 
-dag = DAG('example_python_operator_airflow',
-            max_active_runs=3,
-            catchup=True,
-            schedule_interval=None,
-            start_date=days_ago(2),
-            default_args=default_args)
+    from colorama import Back, Fore, Style
 
-with dag:
+    print(Fore.RED + 'some red text')
+    print(Back.GREEN + 'and with a green background')
+    print(Style.DIM + 'and in dim text')
+    print(Style.RESET_ALL)
+    for _ in range(10):
+        print(Style.DIM + 'Please wait...', flush=True)
+        sleep(10)
+    print('Finished')
 
-    start = DummyOperator(task_id='start')
 
-    for i in range(0,2):
-
-        t1 = PythonOperator( 
-            task_id='python_function_af_{0}'.format(i),
-            python_callable=sample_python_function,
-            dag=dag)
-
-        start >> t1
+virtualenv_task = PythonVirtualenvOperator(
+    task_id="virtualenv_python",
+    python_callable=callable_virtualenv,
+    requirements=["colorama==0.4.0"],
+    system_site_packages=False,
+    dag=dag,
+)
+# [END howto_operator_python_venv]
